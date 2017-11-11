eaf9b2309abcd333664cfa10d2dc55f411966297
Furkan Tokac <furkantokac34@gmail.com>
2017-11-11 12:58:31 +0300

    CNG315 Assignment1 is created.

---
+++ CNG315-Algorithms/1-Assignment1/Assignment1/q1.c
@@ -0,0 +1,17 @@
+/*
+ * Title     : Question1 V1.0
+ * Developer : Furkan TOKAC
+ * Compiler  : gcc version 5.4.0
+ * OS        : KDE Neon LTS 5.8
+ * IDE       : Qt Creator 4.4.1
+ * License   : The MIT License (MIT)
+ * Indent    : 4 spaces
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
