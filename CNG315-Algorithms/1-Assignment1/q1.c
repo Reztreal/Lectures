71cf7ee7d145daba97e790adbcbde470cbbb8559
Furkan Tokac <furkantokac34@gmail.com>
2017-11-17 01:03:32 +0300

    Cng315 Assignment1 is updated.

--- CNG315-Algorithms/1-Assignment1/q1.c
+++ CNG315-Algorithms/1-Assignment1/q1.c
@@ -1,6 +1,7 @@
 /*
  * Title     : Question1 V1.0
- * Developer : Furkan TOKAC
+ * Developer : Furkan TOKAC ftokac@acm.org
+ * StudentID : e2016145
  * Compiler  : gcc version 5.4.0
  * OS        : KDE Neon LTS 5.8
  * IDE       : Qt Creator 4.4.1
@@ -9,9 +10,223 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define TRUE 1
+#define FALSE 0
+#define SIZE 100
+
+
+// Coordinate structure to use with coordinates
+struct Coordinate {
+    int x;
+    int y;
+    int z;
+    int val;
+};
+typedef struct Coordinate Coordinate;
+
+
+// Secure file open function just opens file by handling
+// basic checks like if the file exist.
+FILE*
+sFopen(const char *fileName,
+       const char *mode)
 {
-    printf("Hello World!\n");
+    FILE* fo;
+    fo = fopen(fileName, mode);
+    if( !fo ) {
+        printf("[!] File is not found. Please check file name or file.\n");
+        exit(EXIT_FAILURE);
+    }
+    return fo;
+}
+
+
+
+// ---> Coordinate functions
+
+// cord = cord1;
+void
+equalizeCords(Coordinate *cord,
+              const Coordinate cord1)
+{
+    cord->x = cord1.x;
+    cord->y = cord1.y;
+    cord->z = cord1.z;
+    cord->val = cord1.val;
+}
+
+// cord = (x,y,z) as x,y and z are coordinates
+void
+cordEquals(Coordinate *cord,
+           const int x,
+           const int y,
+           const int z)
+{
+    cord->x = x;
+    cord->y = y;
+    cord->z = z;
+}
+
+// cord1 == cord2
+int
+areCordsEqual(const Coordinate cord1,
+              const Coordinate cord2)
+{
+    if(cord1.x == cord2.x)
+        if(cord1.y == cord2.y)
+            if(cord1.z == cord2.z)
+                return TRUE;
+    return FALSE;
+}
+// cord1 == (x,y,z)
+int
+isCordEqual(const Coordinate cord1,
+            const int x,
+            const int y,
+            const int z)
+{
+    if(cord1.x == x)
+        if(cord1.y == y)
+            if(cord1.z == z)
+                return TRUE;
+    return FALSE;
+}
+// Prints the cord in the format "X Y Z VALUE\n"
+void
+printCord(const Coordinate cord)
+{
+    printf("%d %d %d %d\n", cord.x, cord.y, cord.z, cord.val);
+}
+
+
+
+// ---> ALGORITHMS
+
+/**
+ * Version 2
+ * Complexity : x*(y>z?y:z)
+ *
+ * In this algorithm, in each loop, one layer is calculated
+ * as 2d rectangle. Thats why its more efficient than v1.
+ */
+int
+partA_v2(const int values[][SIZE][SIZE],
+         const Coordinate cord1,
+         const Coordinate cord2)
+{
+    int sum=0;          // results will be stored here
+    Coordinate cordRef; // Reference point. This will be used in algorithm.
+    Coordinate cord;    // Variable to use temporary value holder
+
+    equalizeCords(&cordRef, cord1); // means cordRef = cord1
+
+    while(cordRef.x <= cord2.x)
+    {
+        for(cord.y=cordRef.y; cord.y<=cord2.y; cord.y+=1)
+            sum += values[cordRef.x][cord.y][cordRef.z];
+        cord.y = cordRef.y; // reset cord.y becuase we used it
+
+        for(cord.z=cordRef.z+1; cord.z<=cord2.z; cord.z+=1)
+            sum += values[cordRef.x][cordRef.y][cord.z];
+        cord.z = cordRef.z; // reset cord.z becuase we used it
+
+        cordRef.y += 1;
+        cordRef.z += 1;
+
+        // move the reference point forward
+        if(cordRef.y>cord2.y || cordRef.z>cord2.z )
+            cordEquals(&cordRef, cordRef.x+1, cord1.y, cord1.z);
+    }
+
+    return sum;
+}
+
+/**
+ * Version 1
+ * Complexity : x*y*z
+ *
+ * This algorithm is using the "boxsum" array
+ * that is produced in part B.
+ */
+int
+partA_v1(const int values[][SIZE][SIZE],
+         const Coordinate cord1,
+         const Coordinate cord2)
+{
+    Coordinate cord;    // Variable to use temporary value holder
+    int sum=0;          // results will be stored here
+
+    for(cord.x=cord1.x; cord.x<=cord2.x; cord.x+=1)
+        for(cord.y=cord1.y; cord.y<=cord2.y; cord.y+=1)
+            for(cord.z=cord1.z; cord.z<=cord2.z; cord.z+=1)
+                sum += values[cord.x][cord.y][cord.z];
+
+    return sum;
+}
+
+
+
+// ---> Main program
+
+// Fill "values" and "dimensions" with values in "fileName" file.
+void
+loadValuesFromFile(const char *fileName,
+                   int values[][SIZE][SIZE],
+                   Coordinate *dimensions)
+{
+    Coordinate cord; // Variable to use temporary value holder
+
+    FILE *fo = sFopen(fileName, "r");
+
+    // Get dimensions from first line as "x y z \n"
+    fscanf(fo, "%d %d %d \n", &(dimensions->x), &(dimensions->y), &(dimensions->z));
+
+    // Get values for each coordinate
+    while( fscanf(fo, "%d %d %d %d \n", &cord.x, &cord.y, &cord.z, &cord.val) != -1 )
+        values[cord.x][cord.y][cord.z] = cord.val;
+    fclose(fo);
+}
+
+// PartB : Fill "boxsum" with sums by using algorithms
+void
+fillBoxsumArray(int boxsum[][SIZE][SIZE],
+                const int values[][SIZE][SIZE],
+                const Coordinate dimensions)
+{
+    Coordinate cord;        // Variable to use temporary value holder
+    Coordinate cordZero;    // Will be used to use written algorithms in part A
+
+    cordEquals(&cordZero, 0,0,0);
+    cordEquals(&cord, 0,0,0);
+
+    for(cord.x=0; cord.x<=dimensions.x; cord.x+=1)
+        for(cord.y=0; cord.y<=dimensions.y; cord.y+=1)
+            for(cord.z=0; cord.z<=dimensions.z; cord.z+=1)
+                boxsum[cord.x][cord.y][cord.z] = partA_v2(values, cordZero, cord);
+}
+
+
+int
+main2()
+{
+    int values[SIZE][SIZE][SIZE];
+    int boxsum[SIZE][SIZE][SIZE];
+
+    Coordinate dim;     // Dimensions of the box. (first line)
+    Coordinate cord1;   // User input min (x1,y1,z1)
+    Coordinate cord2;   // User input max (x2,y2,z2)
+
+    loadValuesFromFile("values.txt", values, &dim);
+    fillBoxsumArray(boxsum,values,dim);
+
+    // Also by entering the values below, program can easily be tested.
+    //cordEquals(&cord1, 0,0,1);
+    //cordEquals(&cord2, 4,4,4);
+
+    scanf("(%d,%d,%d) (%d,%d,%d)", &cord1.x, &cord1.y, &cord1.z, &cord2.x, &cord2.y, &cord2.z);
+    printf("The sum of pixels : %d\n", partA_v2(values, cord1, cord2));
+
     return 0;
 }
--- CNG315-Algorithms/1-Assignment1/q2.c
+++ CNG315-Algorithms/1-Assignment1/q2.c
@@ -1,6 +1,7 @@
 /*
  * Title     : Question2 V1.0
- * Developer : Furkan TOKAC
+ * Developer : Furkan TOKAC ftokac@acm.org
+ * StudentID : e2016145
  * Compiler  : gcc version 5.4.0
  * OS        : KDE Neon LTS 5.8
  * IDE       : Qt Creator 4.4.1
@@ -9,9 +10,171 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+#define SIZE 1000
+
+// fflush(stdin) has problem in linux
+void flush()
 {
-    printf("Hello World!\n");
+    while(getchar() != '\n');
+}
+
+// Prints array in the format "1 2 3 4 \n"
+void
+printSortedArray(const int array[],
+                 const int size)
+{
+    int leftest=0, i=0;
+    for(leftest=SIZE/2; array[leftest]!=-1; leftest--); //find leftest
+
+    for(i=leftest+1; array[i]!=-1; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
+
+// Creates "distorted" array by using "elements"
+void
+distortArray(const int elements[],
+             const int pivot,
+             const int size,
+             int distorted[])
+{
+    int i=0;
+    int left=pivot-1, right=pivot+1;
+
+    distorted[0] = elements[pivot];
+
+    for(i=1; i<size; i++)
+    {
+        if(right > size)
+        {
+            for(; left>0; left--, i++)
+                distorted[i] = elements[left];
+            break;
+        }
+
+        if(left < 0)
+        {
+            for(; right<size; right++, i++)
+                distorted[i] = elements[right];
+            break;
+        }
+
+        distorted[i] = elements[left];
+        i++;
+        distorted[i] = elements[right];
+
+        left -= 1;  // go one left
+        right += 1; // go one right
+    }
+}
+
+
+/*
+ * Prints array in the format "1 2 3 4 \n"
+ * This is algorithm that prints the sorted array.
+ */
+void
+printArray(const int array[],
+           const int size)
+{
+    int i=0;
+    for(i=0; i<size; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
+
+// Sorts the "array" an save it in "sorted"
+void
+mySortingAlgorithm(const int array[],
+                   const int size,
+                   int sorted[])
+{
+    int leftestIndex = 0, rightestIndex = 0;
+    const int middleIndex = SIZE/2; // SIZE/2 = 500 in my case
+
+    // place the middle value
+    sorted[ middleIndex ] = array[0];
+    leftestIndex = middleIndex;
+    rightestIndex = middleIndex;
+
+    for(int i=1; i<size; i++)
+    {
+        if( array[i]>sorted[middleIndex] )
+        {
+            if( array[i]>sorted[rightestIndex] )
+            {
+                rightestIndex++;
+                sorted[rightestIndex] = array[i];
+            }
+        }
+        else // means new value is smaller than middle value
+        {
+            if( array[i]<sorted[leftestIndex] )
+            {
+                leftestIndex--;
+                sorted[leftestIndex] = array[i];
+            }
+        }
+    }
+
+    sorted[leftestIndex-1] = -1; // means end of the left part
+    sorted[rightestIndex+1] = -1; // means end of the right part
+}
+
+
+
+int
+main()
+{
+    int numOfElems = 0, i=0;
+    int elements[SIZE];
+
+    srand(time(NULL));
+
+
+    // ---> Number of elements input from user
+    printf("How many elements [10..2000]: ");
+    scanf("%d", &numOfElems);
+
+    while(numOfElems<10 || numOfElems>2000)
+    {
+        printf("ERROR! Please enter size between 10 and 2000: ");
+        scanf("%d", &numOfElems);
+    }
+    flush();
+
+
+    // ---> Elements from user
+    printf("Enter the array elements:  ");
+    for(i=0; i<numOfElems; i++)
+        scanf("%d", &(elements[i]));
+    flush();
+
+
+    // ---> Distort array
+    int distorted[SIZE];
+    int pivot = 3 + rand()%(numOfElems-4);
+    printf("Pivot is chosen as the element with index %d which is %d\n",pivot, elements[pivot]);
+    distortArray(elements, pivot, numOfElems, distorted);
+
+    printf("Distorted array: ");
+    printArray(distorted, numOfElems);
+
+    // ---> My cool sorting algorithm
+    int sorted[SIZE];
+    mySortingAlgorithm(distorted, numOfElems, sorted);
+
+    // ---> Print the array
+    printf("Sorted array   : ");
+    printSortedArray(sorted, numOfElems);
+
+
+    printf("\n\n");
     return 0;
 }
